297204de53b59e3b48fe69926a15bb295a339b86
mahmoud <mahmoud.sidky@recapet.com>
2024-02-15 02:37:13 +0200

    sow all images in website .

--- app/Utils/file_path.php
+++ app/Utils/file_path.php
@@ -1,4 +1,5 @@
 <?php
+
 use App\Enums\GlobalConstant;
 
 if (!function_exists('productImagePath')) {
@@ -11,7 +12,7 @@ if (!function_exists('productImagePath')) {
 if (!function_exists('getValidImage$path')) {
     function getValidImage($path, $type = null, $source = null): string
     {
-
+        $path = str_replace('app/public/', '', $path);
         $givenPath = asset($path);
         if ($source) {
             return is_file($path) ? $givenPath : $source;
@@ -19,98 +20,98 @@ if (!function_exists('getValidImage$path')) {
 
         if ($type == 'backend-basic') {
             return is_file($path) ? $givenPath : asset('public/assets/back-end/img/placeholder/placeholder-1-1.png');
-        }else if ($type == 'backend-brand') {
+        } else if ($type == 'backend-brand') {
             return is_file($path) ? $givenPath : asset('public/assets/back-end/img/placeholder/brand.png');
-        }else if ($type == 'backend-banner') {
+        } else if ($type == 'backend-banner') {
             return is_file($path) ? $givenPath : asset('public/assets/back-end/img/placeholder/placeholder-4-1.png');
-        }else if ($type == 'backend-category') {
+        } else if ($type == 'backend-category') {
             return is_file($path) ? $givenPath : asset('public/assets/back-end/img/placeholder/category.png');
-        }else if ($type == 'backend-logo') {
+        } else if ($type == 'backend-logo') {
             return is_file($path) ? $givenPath : asset('public/assets/back-end/img/placeholder/placeholder-4-1.png');
-        }else if ($type == 'backend-product') {
+        } else if ($type == 'backend-product') {
             return is_file($path) ? $givenPath : asset('public/assets/back-end/img/placeholder/product.png');
-        }else if ($type == 'backend-profile') {
+        } else if ($type == 'backend-profile') {
             return is_file($path) ? $givenPath : asset('public/assets/back-end/img/placeholder/user.png');
-        }else if ($type == 'backend-payment') {
+        } else if ($type == 'backend-payment') {
             return is_file($path) ? $givenPath : asset('public/assets/back-end/img/placeholder/placeholder-4-1.png');
-        }else if ($type == 'product') {
+        } else if ($type == 'product') {
             if (theme_root_path() == 'theme_aster') {
                 return is_file($path) ? $givenPath : theme_asset('assets/img/placeholder/placeholder-1-1.png');
-            }elseif (theme_root_path() == 'theme_fashion') {
+            } elseif (theme_root_path() == 'theme_fashion') {
                 return is_file($path) ? $givenPath : theme_asset('assets/img/placeholder/placeholder-1-1.png');
-            }else {
+            } else {
                 return is_file($path) ? $givenPath : asset('public/assets/front-end/img/placeholder/placeholder-1-1.png');
             }
-        }else if ($type == 'avatar') {
+        } else if ($type == 'avatar') {
             if (theme_root_path() == 'theme_aster') {
                 return is_file($path) ? $givenPath : theme_asset('assets/img/placeholder/user.png');
-            }else if (theme_root_path() == 'theme_fashion') {
+            } else if (theme_root_path() == 'theme_fashion') {
                 return is_file($path) ? $givenPath : theme_asset('assets/img/placeholder/user.png');
-            }else {
+            } else {
                 return is_file($path) ? $givenPath : asset('public/assets/front-end/img/placeholder/user.png');
             }
-        }else if ($type == 'banner') {
+        } else if ($type == 'banner') {
             if (theme_root_path() == 'theme_aster') {
                 return is_file($path) ? $givenPath : theme_asset('assets/img/placeholder/placeholder-2-1.png');
-            }else if (theme_root_path() == 'theme_fashion') {
+            } else if (theme_root_path() == 'theme_fashion') {
                 return is_file($path) ? $givenPath : theme_asset('assets/img/placeholder/placeholder-2-1.png');
-            }else {
+            } else {
                 return is_file($path) ? $givenPath : asset('public/assets/front-end/img/placeholder/placeholder-2-1.png');
             }
-        }else if ($type == 'wide-banner') {
+        } else if ($type == 'wide-banner') {
             if (theme_root_path() == 'theme_aster') {
                 return is_file($path) ? $givenPath : theme_asset('assets/img/placeholder/placeholder-4-1.png');
-            }else if (theme_root_path() == 'theme_fashion') {
+            } else if (theme_root_path() == 'theme_fashion') {
                 return is_file($path) ? $givenPath : theme_asset('assets/img/placeholder/placeholder-4-1.png');
-            }else {
+            } else {
                 return is_file($path) ? $givenPath : asset('public/assets/front-end/img/placeholder/placeholder-4-1.png');
             }
-        }else if ($type == 'brand') {
+        } else if ($type == 'brand') {
             if (theme_root_path() == 'theme_aster') {
                 return is_file($path) ? $givenPath : theme_asset('assets/img/placeholder/placeholder-2-1.png');
-            }else if (theme_root_path() == 'theme_fashion') {
+            } else if (theme_root_path() == 'theme_fashion') {
                 return is_file($path) ? $givenPath : theme_asset('assets/img/placeholder/placeholder-2-1.png');
-            }else {
+            } else {
                 return is_file($path) ? $givenPath : asset('public/assets/front-end/img/placeholder/placeholder-1-1.png');
             }
-        }else if ($type == 'category') {
+        } else if ($type == 'category') {
             if (theme_root_path() == 'theme_aster') {
                 return is_file($path) ? $givenPath : theme_asset('assets/img/placeholder/placeholder-1-1.png');
-            }else if (theme_root_path() == 'theme_fashion') {
+            } else if (theme_root_path() == 'theme_fashion') {
                 return is_file($path) ? $givenPath : theme_asset('assets/img/placeholder/placeholder-1-1.png');
-            }else {
+            } else {
                 return is_file($path) ? $givenPath : asset('public/assets/front-end/img/placeholder/placeholder-1-1.png');
             }
-        }else if ($type == 'logo') {
+        } else if ($type == 'logo') {
             if (theme_root_path() == 'theme_aster') {
                 return is_file($path) ? $givenPath : theme_asset('assets/img/placeholder/placeholder-4-1.png');
-            }else if (theme_root_path() == 'theme_fashion') {
+            } else if (theme_root_path() == 'theme_fashion') {
                 return is_file($path) ? $givenPath : theme_asset('assets/img/placeholder/placeholder-4-1.png');
-            }else {
+            } else {
                 return is_file($path) ? $givenPath : asset('public/assets/front-end/img/placeholder/placeholder-4-1.png');
             }
-        }else if ($type == 'shop') {
+        } else if ($type == 'shop') {
             if (theme_root_path() == 'theme_aster') {
                 return is_file($path) ? $givenPath : theme_asset('assets/img/placeholder/shop.png');
-            }else if (theme_root_path() == 'theme_fashion') {
+            } else if (theme_root_path() == 'theme_fashion') {
                 return is_file($path) ? $givenPath : theme_asset('assets/img/placeholder/shop.png');
-            }else {
+            } else {
                 return is_file($path) ? $givenPath : asset('public/assets/front-end/img/placeholder/shop.png');
             }
-        }else if ($type == 'shop-banner') {
+        } else if ($type == 'shop-banner') {
             if (theme_root_path() == 'theme_aster') {
                 return is_file($path) ? $givenPath : theme_asset('assets/img/placeholder/placeholder-4-1.png');
-            }else if (theme_root_path() == 'theme_fashion') {
+            } else if (theme_root_path() == 'theme_fashion') {
                 return is_file($path) ? $givenPath : theme_asset('assets/img/placeholder/placeholder-4-1.png');
-            }else {
+            } else {
                 return is_file($path) ? $givenPath : asset('public/assets/front-end/img/placeholder/seller-banner.png');
             }
-        }else{
+        } else {
             if (theme_root_path() == 'theme_aster') {
                 return is_file($path) ? $givenPath : theme_asset('assets/img/placeholder/placeholder-2-1.png');
-            }else if (theme_root_path() == 'theme_fashion') {
+            } else if (theme_root_path() == 'theme_fashion') {
                 return is_file($path) ? $givenPath : theme_asset('assets/img/placeholder/placeholder-2-1.png');
-            }else {
+            } else {
                 return is_file($path) ? $givenPath : asset('public/assets/front-end/img/placeholder/placeholder-2-1.png');
             }
         }
